58141ad44f404490ad5c7b5322aa1c82e7fb5064
webdevandres <maciasandres02@gmail.com>
2020-09-04 08:47:46 -0500

    use factory function to create object

--- library_task.js
+++ library_task.js
@@ -1,20 +1,39 @@
 "use strict";
 
 //constructor function
-var Task = function(task) {
-    this.text = task;
-};
+// var Task = function(task) {
+//     this.text = task;
+// };
 
 //below two methods are added to the prototype object
-Task.prototype.isValid = function() {
-    if (this.text === "") {
-        return false;
-    } else {
-        return true;
+// Task.prototype.isValid = function() {
+//     if (this.text === "") {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// };
+
+// Task.prototype.toString = function(){
+//     //capitalize the first letter of the task text
+//     return this.text.capitalize();
+// };
+
+var taskPrototype = {
+    isValid: function() {
+        if(this.text === "") {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    toString: function() {
+        return this.text.capitalize();
     }
 };
 
-Task.prototype.toString = function(){
-    //capitalize the first letter of the task text
-    return this.text.capitalize();
-};
+var getTask = function(taskText) {
+    var task = Object.create(taskPrototype);
+    task.text = taskText;
+    return task;
+}
--- task_list.js
+++ task_list.js
@@ -6,7 +6,8 @@ var $ = function(id) {
 
 var addToTaskList = function() {
     var taskTextBox = $("task");
-    var newTask = new Task(taskTextBox.value);
+    // var newTask = new Task(taskTextBox.value);
+    var newTask = getTask(taskTextBox.value);
     if(newTask.isValid()) {
         taskList.add(newTask).save().display();
         taskTextBox.value = "";
@@ -26,11 +27,24 @@ var deleteFromTaskList = function() {
     $("task").focus();
 };
 
+var editTaskListItem = function() {
+    var newText = prompt("Please enter new text", taskList.tasks[this.title]);
+    // var editedTask = new Task(newText);
+    var editedTask = getTask(newText);
+    if(editedTask.isValid()) {
+        taskList.edit(this.title, editedTask).save().display();
+        $("task").focus();
+    } else {
+        alert("Please enter a valid task");
+    }
+};
+
 window.onload = function() {
     $("add_task").onclick = addToTaskList;
     $("clear_tasks").onclick = clearTaskList;
 
     taskList.deleteClickHandler = deleteFromTaskList;
+    taskList.editClickHandler = editTaskListItem;
     taskList.displayDiv = $("tasks");
 
     taskList.load().display();
